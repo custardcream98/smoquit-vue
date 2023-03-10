d4c1083e3aa472c78410772c322c694370832675
shiwoo.park <custardcream@kakao.com>
2023-03-11 01:56:38 +0900

    Config: firebase initialize

---
+++ src/constants/firestore.ts
@@ -0,0 +1,13 @@
+const DOC_CAMPAIGNS_BY_USER = "campaigns_by_user" as const;
+const DOC_CAMPAIGNS = "campaigns" as const;
+const DOC_PROFILE = "profile" as const;
+const DOC_FREEBOARD_POST = "freeboard_post" as const;
+const DOC_COMMENT = "comment" as const;
+
+export {
+  DOC_CAMPAIGNS_BY_USER,
+  DOC_CAMPAIGNS,
+  DOC_PROFILE,
+  DOC_FREEBOARD_POST,
+  DOC_COMMENT
+};
