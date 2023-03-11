5c406f078f4d37e569c3e3dc26a997b3a18fa00c
shiwoo.park <custardcream@kakao.com>
2023-03-12 02:21:41 +0900

    Feat: HomeView에 버튼 및 카운터 추가

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import TheGreetingMessage from '@/components/Layout/TheGreetingMessage.vue';
+import LoadableButton from '@/components/LoadableButton.vue';
+import Counter from '@/components/Counter/index.vue';
 
+const isClicked = ref<boolean>(false);
 
+const handleClick = (_:MouseEvent) => {
+  isClicked.value = true;
+};
 </script>
 
 <template>
   <TheGreetingMessage />
+  <LoadableButton v-if="!isClicked" :loading="false" :disabled="false" @click="handleClick($event)" >노담 시작하기</LoadableButton>
+  <Counter v-else/>
 </template>
