4e092e006073d2b9cf7597ca63184916d543dfb9
shiwoo.park <custardcream@kakao.com>
2023-03-12 11:25:01 +0900

    Feat: LoginHeader 컴포넌트

---
+++ src/components/LoginHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <img :src="Logo" alt="Smoquit 로고" class="mt-24 block h-12 w-12" />
+    <h1 class="text-gradient mt-9 text-4xl font-bold pc:text-6xl">Smoquit</h1>
+    <p class="text-gradient mt-3 block text-xl pc:text-4xl">
+      나는 네가 <stong class="font-bold">#노담</stong>이면_좋겠어
+      <strong class="font-bold">#진심 #노담</strong>
+    </p>
+  </header>
+</template>
+
+<script lang="ts" setup>
+import Logo from "@/assets/SmoquitLogo.png";
+</script>
