3f026d30092e05864c48f69c4b6341d36aed87f4
shiwoo.park <custardcream@kakao.com>
2023-03-12 11:24:24 +0900

    Config: tailwind custom break point

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
-/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      pc: "768px"
+    },
     extend: {}
   },
   plugins: []
