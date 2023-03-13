669e1a677e18a43e42d9f8306e9c6da845ef6c29
shiwoo.park <custardcream@kakao.com>
2023-03-13 10:53:12 +0900

    Fix: LoginHeader 오타

--- src/components/LoginHeader.vue
+++ src/components/LoginHeader.vue
@@ -3,7 +3,7 @@
     <img :src="Logo" alt="Smoquit 로고" class="mt-24 block h-12 w-12" />
     <h1 class="text-gradient mt-9 text-4xl font-bold pc:text-6xl">Smoquit</h1>
     <p class="text-gradient mt-3 block text-xl pc:text-4xl">
-      나는 네가 <stong class="font-bold">#노담</stong>이면_좋겠어
+      나는 네가 <strong class="font-bold">#노담</strong>이면_좋겠어
       <strong class="font-bold">#진심 #노담</strong>
     </p>
   </header>
