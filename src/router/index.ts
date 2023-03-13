84a5afd58e33562af9ed618ac5a46b108a5f3ac7
shiwoo.park <custardcream@kakao.com>
2023-03-13 10:18:27 +0900

    Feat: router.beforeEach로 로그인 여부 확인

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import HomeView from "@/views/HomeView.vue";
+import useIsAuthenticated from "@/compositions/useIsAuthenticated";
 import ROUTE from "@/constants/route";
+import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,14 +10,42 @@ const router = createRouter({
     {
       path: ROUTE.HOME,
       name: "home",
-      component: HomeView
+      component: HomeView,
     },
     {
       path: ROUTE.HISTORY,
       name: "history",
-      component: () => import("@/views/HistoryView.vue")
-    }
-  ]
+      component: () => import("@/views/HistoryView.vue"),
+    },
+    {
+      path: ROUTE.LOGIN,
+      name: "login",
+      component: () => import("@/views/LoginView.vue"),
+    },
+  ],
+});
+
+/** 로그인 확인 */
+router.beforeEach((to, _, next) => {
+  const { isAuthenticated } = useIsAuthenticated();
+  const isToLogin = to.name === "login";
+
+  if (isToLogin && !isAuthenticated) {
+    next();
+    return;
+  }
+
+  if (isToLogin && isAuthenticated) {
+    next({ name: "home" });
+    return;
+  }
+
+  if (!isAuthenticated) {
+    next({ name: "login" });
+    return;
+  }
+
+  next();
 });
 
 export default router;
